2677333c2f79d6ec7c82768e581e1025358d09f1
Carlos Javier <carlos.riverarivera@protonmail.ch>
2022-01-20 12:22:45 +0000

    fixed scratch.py to work with the new version of main() that gets fed a founder organism, and it also now runs a chi-square test that sees if the observed sequence differences match to the expected sequence differences, given a quartet evolution

--- scratch.py
+++ scratch.py
@@ -1,3 +1,25 @@
 import retikulos as rt
 import numpy as np
 rez=rt.main()
+def parse_rez(rez):
+     expvals=[2.49,4.98,4.98,4.98,4.98,9.96,9.96,9.96,9.96]
+     for k in range(len(rez)):
+         pop=rez[k]
+         outvect=np.ndarray(pop.size,dtype=object)
+         exp=expvals[k]
+         chisqvect=np.ndarray(pop.size,dtype=object)
+         df=pop.size-1
+         print(f"Degrees of freedom: {df} for population {k}. Expected avg differences {exp}.")
+         dftab=np.ndarray(len(rez),dtype=int)
+         dftab[k]=df
+         chivaltab=np.ndarray(len(rez))
+         for i in range(pop.size):
+             obs=np.where(founder[0][1] != pop[i][1])[0].size
+             outvect[i]=obs
+             chisqvect[i]=((obs-exp)**2)/exp
+         chisq=sum(chisqvect)
+         chivaltab[k]=chisq
+         meandiffs=np.mean(outvect)
+         print(k,meandiffs,pval)
+     return
+parse_rez(rez)
