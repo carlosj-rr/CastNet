364cdfe5f346d9be2c62116cebfc515f14c87e9e
Djordje <djgr@itu.dk>
2022-02-22 16:40:00 +0100

    I made the generic W_guess initialization and explained what's happening with comments.

--- lts_cjrr.py
+++ lts_cjrr.py
@@ -1,8 +1,10 @@
 import jax.numpy as np
 from jax import grad, jit
 from jax import random
+import time
 
 LR = 0.01
+
 
 def loss(X, W, y):
     """Loss function for a linear regression. A forward pass of our model.
@@ -20,45 +22,48 @@ def loss(X, W, y):
     return ((y_hat - y) ** 2).mean()  # Return cost.
 
 
-def gradient_descent(W, inputs, targets):
+def gradient_descent(W_guess, inputs, targets):
     gradient = jit(grad(loss, argnums=1))
-    print(f"Start loss {loss(inputs, W, targets)}")
+    print(f"Start loss {loss(inputs, W_guess, targets)}")
     for epoch in range(10000):
-        W -= LR * gradient(inputs, W, targets)
-    return W
+        W_guess -= LR * gradient(inputs, W_guess, targets)
+    return W_guess
 
 
-def find_linear_transform(inputs, targets, W):
-    #W = np.array([
-    #    [-1., 1., -1.],
-    #    [1., 1., -1.],
-    #    [-1., -1., 1.],
-    #])
-    W = gradient_descent(W, inputs, targets)
+def find_linear_transform(inputs, targets):
+    assert inputs.shape == targets.shape, "Inputs and targets have missmatching shapes"
+    W_shape = (inputs.shape[1], inputs.shape[1])
+    key = random.PRNGKey(0)
+    W_guess = random.normal(key, W_shape)  # <--- This is my initial incorrect guess about what the true W is.
+
+    # This method will iteratively try to fix the initial guess to match the input/output pairs by minimising the
+    # loss/mismatch.
+    W_guess = gradient_descent(W_guess, inputs, targets)
 
     print(f"Final loss {loss(inputs, W, targets)}")
-    return W
-
-X = np.array([
-    [4., 7., 3.],
-    [1., 8., 1.],
-    [-5., -6., -1],
-    [3., -1., -2.],
-    [0., 9., 6.]
-])
-
-W = np.array([
-    [4., 7., 3.],
-    [1., 8., -1.],
-    [-5., -6., 11.],
-])
-
-def main(X,W):
-    y = X.dot(W)
-    print(y)
-    approximated_W = find_linear_transform(X, y, W)
-    print(f"Original W matrix {W}")
-    print(f"final W matrix {approximated_W}")
+    return W_guess
 
 
-main()
+def main(X, true_W, y):
+    W_guess = find_linear_transform(X, y)  # <--- this function should not take the correct W in any case, LoL
+    print(f"Original W matrix {true_W}")
+    print(f"final W matrix {W_guess}")
+
+
+if __name__ == "__main__":
+    X = np.array([  # These are my input vectors arranged in a matrix
+        [4., 7., 3.],  # <--- ...
+        [1., 8., 1.],  # <--- ...
+        [-5., -6., -1],  # <--- ...
+        [3., -1., -2.],
+        [0., 9., 6.]
+    ])
+
+    true_W = np.array([  # This is a matrix I'm trying to learn, in this case I know the correct one since I defined
+        [4., 7., 3.],  # it here. Normally, I don't have this matrix and I'm trying to infer it with this method
+        [1., 8., -1.],
+        [-5., -6., 11.],
+    ])
+    y = X.dot(true_W)  # These are the outputs of the transformation having the inputs from earlier. This is something
+    #  I will have, and that I will use to infer the W matrix (the unknown transformation)
+    main(X, true_W, y)
