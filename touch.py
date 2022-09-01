d69b6bb39ff41c1e48c495aeccf3cbe4819631b0
Carlos Javier <carlos.riverarivera@protonmail.ch>
2022-09-01 18:19:58 +0200

    touch checks for diffs in GRNs as well.

--- touch.py
+++ touch.py
@@ -2,4 +2,5 @@ import numpy as np
 gen10=np.load("Generation_10_branch_1.npy",allow_pickle=True)
 gen20=np.load("Generation_20_branch_1.npy",allow_pickle=True)
 
-print(f"Genomes are {np.multiply(100,np.sum(gen10[0][1] != gen20[0][1])/gen[0][1].size)}% different")
+print(f"Genomes are {np.multiply(100,np.sum(gen10[0][1] != gen20[0][1])/gen10[0][1].size)}% different")
+print(f"GRNs are {np.multiply(100,np.sum(gen10[0][3] != gen20[0][3])/gen10[0][3].size)}% different")
