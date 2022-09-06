b47a9b78dd5b92f0e80c5924bd69c30623d4c840
Carlos Javier <carlos.riverarivera@protonmail.ch>
2022-09-06 19:50:22 +0200

    fiddling for profiling

--- scratch.py
+++ scratch.py
@@ -1,2 +1,16 @@
 import retikulos as rt
-rez=rt.main_parallel()
+import cProfile, pstats, io
+np=rt.np
+from pstats import SortKey
+founder=rt.founder_miner()
+pr = cProfile.Profile()
+pr.enable()
+new_pop=rt.grow_pop(founder,100)
+pr.disable()
+s = io.StringIO()
+sortby = SortKey.CUMULATIVE
+ps = pstats.Stats(pr, stream=s).sort_stats(sortby)
+ps.print_stats()
+textfile=open("profile_growpop_new2.txt","w")
+textfile.write(s.getvalue())
+textfile.close()
