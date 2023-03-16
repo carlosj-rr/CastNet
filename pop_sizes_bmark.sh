2c4c083e8b384772452857363ccc9c6964499136
Carlos Javier <carlos.riverarivera@protonmail.ch>
2023-03-16 16:55:53 +0000

    upload pop size bmark script

--- pop_sizes_bmark.sh
+++ pop_sizes_bmark.sh
@@ -1,5 +1,7 @@
-cd ~/Programs/CastNet/10-1K_PopSize_Bmarks
+#mkdir ~/Programs/CastNet/10-2K_PopSize_Bmarks
+cd ~/Programs/CastNet/10-2K_PopSize_Bmarks
 cp ../CastNet.py ../CastNet_parameters.py ../CastNet_out_funcs.py .
+echo "pop_size,start,stop" > pop_sizes_times_parallel.csv
 for new_pop_size in 10 50 100 200 300 400 500 1000 2000
 do
   cp CastNet_parameters.py CastNet_parameters_${new_pop_size}.py
@@ -7,9 +9,10 @@ do
   old_pop_size=$(egrep -o "pop_size = [0-9]+" CastNet_parameters_${new_pop_size}.py)
   sed -i "s/$old_pop_size/pop_size = $new_pop_size/" CastNet_parameters_${new_pop_size}.py
   sed -i "s/CastNet_parameters/CastNet_parameters_${new_pop_size}/" CastNet_${new_pop_size}.py
-  date +%H,%M,%S > start_stop_${new_pop_size}-indivs.csv
+  start=$(date +%H:%M:%S)
   python CastNet_${new_pop_size}.py ${new_pop_size}_indivsRun
-  date +%H,%M,%S >> start_stop_${new_pop_size}-indivs.csv
+  stop=$(date +%H:%M:%S)
+  echo $new_pop_size,$start,$stop >> pop_sizes_times_parallel.csv 
 done
 cd ~/Programs/CastNet
 echo "Finished second bit successfully"
